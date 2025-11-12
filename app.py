877e4028766a33462de62367ecd965db146c4085
Eli69Pro <elaheh.golrokh@gmail.com>
2025-11-12 21:40:10 +0330

    update app comment

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from src.serving import GradioApp
 
 
 config = OmegaConf.load('private_settings.yaml')
+
+# For huggingface version use this config:
 # config = OmegaConf.create({
 #     "repo_id": os.getenv("REPO_ID"),
 #     "hf_token": os.getenv("HF_TOKEN"),
 #     "file_name": 'model.pt'
 # })
-# config = OmegaConf.merge(base_conf, private_conf)
 
 app = GradioApp(config)
 demo = app.build()
